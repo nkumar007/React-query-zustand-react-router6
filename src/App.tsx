1a8a47429e1d7e7513373aba101abf763e9a82a2
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-25 12:45:58 +0530

    using zustand for global state management

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import { AuthProvider } from "./state-management/auth";
 import { TaskProvider } from "./state-management/task";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <AuthProvider>
       <TaskProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TaskProvider>
--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
-import TasksContext from "./task/tasksContext";
 import LoginStatus from "./auth/LoginStatus";
+import useCounterStore from "./counter/store";
+import TasksContext from "./task/tasksContext";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
