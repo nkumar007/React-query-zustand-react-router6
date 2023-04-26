43e89a37b12e979e000bc989049315e20f149f0b
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-26 12:29:38 +0530

    nested routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,25 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
+import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/users",
-    element: <UserListPage />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
-  },
-  {
-    path: "/users/:id",
-    element: <UserDetailPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "users",
+        element: <UserListPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "users/:id",
+        element: <UserDetailPage />,
+      },
+    ],
   },
 ]);
 
